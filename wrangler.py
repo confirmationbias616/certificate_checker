82bd79a31d05cef04602abbe65be9bf061d59b24
Alex Roy <Alex@AlexMBP.local>
2019-02-23 16:34:53 -0500

    Update wrangler.py

--- wrangler.py
+++ wrangler.py
@@ -165,7 +165,7 @@ def wrangle():
 	    'street_name': get_street_name,
 	    }
 	    for attr in get_address_ops:
-            df[attr] = df['address'].astype('str').apply(get_address_ops[attr])
+	    	df[attr] = df['address'].astype('str').apply(get_address_ops[attr])
 	    for attr in ["title","owner","contractor"]:
 	        df[f'{attr}_acronyms'] = df[attr].apply(get_acronyms)
 	    return df
