4ee0d89b14a9a7da152b05223c2af15979addb8b
Alex Roy <Alex@AlexMBP.local>
2019-02-22 22:06:52 -0500

    fixed import statements

--- matcher.py
+++ matcher.py
@@ -1,17 +1,21 @@
 from fuzzywuzzy import fuzz
 import pandas as pd
 import datetime
-from communicator import (send_email)
+from communicator import communicate
 
 
-df_dilfo = pd.read_csv('./data/clean_dilfo_certs.csv')
-df_web = pd.read_csv(f'./data/clean_web_certs_{datetime.datetime.now().date()}.csv')
+def match():
+	df_dilfo = pd.read_csv('./data/clean_dilfo_certs.csv')
+	df_web = pd.read_csv(f'./data/clean_web_certs_{datetime.datetime.now().date()}.csv')
 
-for i in range(len(df_dilfo)):
-	df_web['contractor_match'] = df_web['contractor'].apply(lambda row: fuzz.ratio(row, df_dilfo.iloc[i]['contractor']))
-	ranked = df_web.sort_values('contractor_match', ascending=False)
-	for i in range(len(ranked)):
-		if ranked.iloc[i]['contractor_match'] > 90:
-			send_email(ranked.iloc[i])
-		else:
-			break
+	for i in range(len(df_dilfo)):
+		df_web['contractor_match'] = df_web['contractor'].apply(lambda row: fuzz.ratio(row, df_dilfo.iloc[i]['contractor']))
+		ranked = df_web.sort_values('contractor_match', ascending=False)
+		for i in range(len(ranked)):
+			if ranked.iloc[i]['contractor_match'] > 90:
+				send_email(ranked.iloc[i])
+			else:
+				break
+
+if __name__=='__main__':
+	match()
--- scraper.py
+++ scraper.py
@@ -5,51 +5,55 @@ import pandas as pd
 import numpy as np
 
 
-pub_date = []
-city = []
-address = []
-title = []
-owner = []
-contractor = []
-engineer = []
+def scrape():
+    pub_date = []
+    city = []
+    address = []
+    title = []
+    owner = []
+    contractor = []
+    engineer = []
 
-url = 'https://canada.constructconnect.com/dcn/certificates-and-notices?perpage=1000&phrase=&sort=publish_date&owner=&contractor=&date=past_7&date_from=&date_to=#results'
+    url = 'https://canada.constructconnect.com/dcn/certificates-and-notices?perpage=1000&phrase=&sort=publish_date&owner=&contractor=&date=past_7&date_from=&date_to=#results'
 
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html, "html.parser")
-
-def get_details(entry):
-    project_details = 'https://canada.constructconnect.com' + entry.find("a")["href"]
-    response = requests.get(project_details)
+    response = requests.get(url)
     html = response.content
-    entry_soup = BeautifulSoup(html, "html.parser")
-    pub_date.append(entry_soup.find("time").get_text())
-    city.append(entry_soup.find("div",{"class":"content-left"}).find("h4").get_text())
-    address.append(entry_soup.find("p",{"class":"print-visible"}).get_text())
-    title.append(entry_soup.find_all("section",{"class":"content"})[3].find("p").get_text())
-    company_soup = entry_soup.find_all("section",{"class":"content"})[4]
-    company_results = {
-        key.get_text():value.get_text() for key, value in zip(
-            company_soup.find_all("dt"), company_soup.find_all("dd"))}
-    lookup = {"Name of Owner": owner, "Name of Contractor": contractor, "Name of Certifier": engineer}
-    for key in list(lookup.keys()):
-        lookup[key].append(company_results.get(key, np.nan))
+    soup = BeautifulSoup(html, "html.parser")
 
-for i, entry in enumerate(soup.find_all("article", {"class":"cards-item"}), 1):
-    print(f'getting entry #{i}...')
-    get_details(entry)
+    def get_details(entry):
+        project_details = 'https://canada.constructconnect.com' + entry.find("a")["href"]
+        response = requests.get(project_details)
+        html = response.content
+        entry_soup = BeautifulSoup(html, "html.parser")
+        pub_date.append(entry_soup.find("time").get_text())
+        city.append(entry_soup.find("div",{"class":"content-left"}).find("h4").get_text())
+        address.append(entry_soup.find("p",{"class":"print-visible"}).get_text())
+        title.append(entry_soup.find_all("section",{"class":"content"})[3].find("p").get_text())
+        company_soup = entry_soup.find_all("section",{"class":"content"})[4]
+        company_results = {
+            key.get_text():value.get_text() for key, value in zip(
+                company_soup.find_all("dt"), company_soup.find_all("dd"))}
+        lookup = {"Name of Owner": owner, "Name of Contractor": contractor, "Name of Certifier": engineer}
+        for key in list(lookup.keys()):
+            lookup[key].append(company_results.get(key, np.nan))
 
-df_web = pd.DataFrame(
-    data={
-        "pub_date": pub_date,
-        "city": city,
-        "address": address,
-        "title": title,
-        "owner": owner,
-        "contractor": contractor,
-        "engineer": engineer
-    }
-)
+    for i, entry in enumerate(soup.find_all("article", {"class":"cards-item"}), 1):
+        print(f'getting entry #{i}...')
+        get_details(entry)
 
-df_web.astype('str').to_csv(f'./data/raw_web_certs_{datetime.datetime.now().date()}.csv', index=False)
+    df_web = pd.DataFrame(
+        data={
+            "pub_date": pub_date,
+            "city": city,
+            "address": address,
+            "title": title,
+            "owner": owner,
+            "contractor": contractor,
+            "engineer": engineer
+        }
+    )
+
+    df_web.astype('str').to_csv(f'./data/raw_web_certs_{datetime.datetime.now().date()}.csv', index=False)
+
+if __name__=="__main__":
+    scrape()
