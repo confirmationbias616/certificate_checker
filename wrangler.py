3b0f931a425981ce2e23f4ed8fa99ab4139d1a99
Alex Roy <Alex@AlexMBP.local>
2019-02-22 22:13:54 -0500

    Revert "fixed import statements"
    This reverts commit e466da9ccfc006d59cec39bc5abe23c1e2285ca1.

--- wrangler.py
+++ wrangler.py
@@ -4,136 +4,137 @@ import numpy as np
 from cleanco import cleanco
 import unidecode
 
+
+def clean_pub_date(raw):
+    date = raw.replace("\n", "").replace(" ", "")
+    return date
+
+def clean_city(raw):
+    city = raw.lower()
+    for variant in [
+        " ste ",
+        " ste. ",
+        " ste-",
+        " st ",
+        " saint ",
+        " sainte ",
+        " st-"
+        ]: city = city.replace(variant," st ")
+    city = city.replace("of the ","")
+    if "of " in city:
+        city = city.split("of ")[1]
+    if ("county, " in city) and ("county, on" not in city):
+        city = city.split("county, ")[1]
+    for sep in (",", " - "):
+        if sep in city:
+            city = city.split(sep)[0]
+    city = city.replace("-"," ")
+    for word in [
+        "city",
+        "county",
+        "municipality",
+        "district",
+        "ward",
+        "township",
+        "greater",
+        "region"
+        ]: city = city.replace(word," ")
+    city = city.rstrip(" ").lstrip(" ")
+    if city.endswith(" on", -3):
+        city = city[:-3]
+    elif "ontario" in city:
+        city = city.strip("ontario")
+    city = "".join([x.rstrip("'s") for x in city.split(" ")])
+    city = city.replace("/","&")
+    return city
+
+def clean_company_name(raw):
+    name = unidecode.unidecode(raw)
+    name = cleanco(name).clean_name()
+    name = name.lower()
+    for stopword in [
+        "of", 
+        "d'",
+        "l'"
+    ]:
+        name = name.replace(stopword,"")
+    name = name.replace("and","&")
+    for punct in ["-", ".", ","]:
+        name = name.replace(punct," ")
+    for punct in ["'"]:
+        name = name.replace(punct,"")
+    if (not name.startswith('s ')) and (not " s " in name):
+        name = " ".join([word.rstrip("s") for word in name.split(" ")])
+    name = "".join([word for word in name.split(" ")])
+    for word in [
+        "constructor",
+        "construction",
+        "contracting",
+        "contractor",
+        "mechanical",
+        "plumbing",
+        "heating",
+        "mech",
+        "electrical",
+        "electric",
+        "development",
+        "interior"
+        "builders",
+        "building",
+        "enterprise",
+        "infrastructure",
+        "management",
+        "excavating",
+        "trucking",
+        "company",
+        "restoration",
+        "service",
+        "servicing",
+        "hvac",
+        "system",
+        "paving",
+        "industrie",
+        "industry",
+        "engineering",
+        "consulting",
+        "consultant",
+        "solution",
+        "commercial",
+        "group",
+        "insulation",
+        "insulators"
+    ]: name = name.replace(word,"")
+    return name
+
+def get_acronyms(raw):
+    try:
+        text = ''.join([x for x in raw if ((not x.isdigit()) and (x not in ',()-:;.&/'))])
+        accronyms =  [x for x in text.split(" ") if len(x)>2 and x.isupper()]
+        if len(accronyms) == 0:
+            return np.nan
+        return accronyms
+    except TypeError:
+        return np.nan
+
+def get_street_number(raw):
+    number = raw.split(' ')[0]
+    try:
+        int(number)
+        return number
+    except ValueError:
+        return np.nan
+
+def get_street_name(raw):
+    name = raw.split(' ')[1]
+    name = unidecode.unidecode(name)
+    name = name.lower()
+    return name
+
+def clean_title(raw):
+    title = raw
+    return title
+
 def wrangle(df):
-    def clean_pub_date(raw):
-        date = raw.replace("\n", "").replace(" ", "")
-        return date
-
-    def clean_city(raw):
-        city = raw.lower()
-        for variant in [
-            " ste ",
-            " ste. ",
-            " ste-",
-            " st ",
-            " saint ",
-            " sainte ",
-            " st-"
-            ]: city = city.replace(variant," st ")
-        city = city.replace("of the ","")
-        if "of " in city:
-            city = city.split("of ")[1]
-        if ("county, " in city) and ("county, on" not in city):
-            city = city.split("county, ")[1]
-        for sep in (",", " - "):
-            if sep in city:
-                city = city.split(sep)[0]
-        city = city.replace("-"," ")
-        for word in [
-            "city",
-            "county",
-            "municipality",
-            "district",
-            "ward",
-            "township",
-            "greater",
-            "region"
-            ]: city = city.replace(word," ")
-        city = city.rstrip(" ").lstrip(" ")
-        if city.endswith(" on", -3):
-            city = city[:-3]
-        elif "ontario" in city:
-            city = city.strip("ontario")
-        city = "".join([x.rstrip("'s") for x in city.split(" ")])
-        city = city.replace("/","&")
-        return city
-
-    def clean_company_name(raw):
-        name = unidecode.unidecode(raw)
-        name = cleanco(name).clean_name()
-        name = name.lower()
-        for stopword in [
-            "of", 
-            "d'",
-            "l'"
-        ]:
-            name = name.replace(stopword,"")
-        name = name.replace("and","&")
-        for punct in ["-", ".", ","]:
-            name = name.replace(punct," ")
-        for punct in ["'"]:
-            name = name.replace(punct,"")
-        if (not name.startswith('s ')) and (not " s " in name):
-            name = " ".join([word.rstrip("s") for word in name.split(" ")])
-        name = "".join([word for word in name.split(" ")])
-        for word in [
-            "constructor",
-            "construction",
-            "contracting",
-            "contractor",
-            "mechanical",
-            "plumbing",
-            "heating",
-            "mech",
-            "electrical",
-            "electric",
-            "development",
-            "interior"
-            "builders",
-            "building",
-            "enterprise",
-            "infrastructure",
-            "management",
-            "excavating",
-            "trucking",
-            "company",
-            "restoration",
-            "service",
-            "servicing",
-            "hvac",
-            "system",
-            "paving",
-            "industrie",
-            "industry",
-            "engineering",
-            "consulting",
-            "consultant",
-            "solution",
-            "commercial",
-            "group",
-            "insulation",
-            "insulators"
-        ]: name = name.replace(word,"")
-        return name
-
-    def get_acronyms(raw):
-        try:
-            text = ''.join([x for x in raw if ((not x.isdigit()) and (x not in ',()-:;.&/'))])
-            accronyms =  [x for x in text.split(" ") if len(x)>2 and x.isupper()]
-            if len(accronyms) == 0:
-                return np.nan
-            return accronyms
-        except TypeError:
-            return np.nan
-
-    def get_street_number(raw):
-        number = raw.split(' ')[0]
-        try:
-            int(number)
-            return number
-        except ValueError:
-            return np.nan
-
-    def get_street_name(raw):
-        name = raw.split(' ')[1]
-        name = unidecode.unidecode(name)
-        name = name.lower()
-        return name
-
-    def clean_title(raw):
-        title = raw
-        return title
-
     clean_ops = {
     'pub_date': clean_pub_date,
     'city': clean_city,
