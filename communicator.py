1aad2cb423819037db0bdbcc03159eeab03b730b
Alex Roy <Alex@AlexMBP.local>
2019-02-23 23:47:34 -0500

    Reworked email content and formatting

--- communicator.py
+++ communicator.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import datetime
 
 def communicate(web_row, dilfo_row):
 	port = 465 # for SSL
@@ -10,15 +11,37 @@ def communicate(web_row, dilfo_row):
 	url = 'https://canada.constructconnect.com/dcn/certificates-and-notices?perpage=1000&phrase=&sort=publish_date&owner=&contractor=&date=past_7&date_from=&date_to=#results'
 
 	def send_email():
+		
+		pud_date = datetime.datetime(
+				*[int(web_row.pub_date.split('-')[x]) for x in range(3)]).date()
+		due_date = lambda delay: pud_date + datetime.timedelta(days=delay)
+		
 		message = (
-		    f"Subject: Alert for possible HB Release!!!"
+		    f"Subject: Alert for Holdback Release on Dilfo Project "
+		    f"#{dilfo_row.job_number} - {dilfo_row.title}"
 		    f"\n\n"
-		    f"See below for details:"
-		    f"{web_row}"
-		    f"\n\n"
-		    f"Go check the website {dilfo_row.cert_url}"
+		    f"Hi {receiver_email.split('.')[0].title()},"
+		    f"\n"
+		    f"You're receiving this e-mail notification because either you or another "
+		    f"Dilfo employee added your project #{dilfo_row.job_number} - "
+		    f"{dilfo_row.title} to the watchlist of upcoming holdback releases."
+		    f"\n"
+		    f"Since this project's certificate was just recently published this past "
+		    f"{datetime.datetime.strftime(pud_date,'%A')} on "
+		    f"{datetime.datetime.strftime(pud_date,'%B %e, %Y')}, a valid holdback "
+		    f"release invoice could be submitted as of:"
+		    f"\n1.\t{datetime.datetime.strftime(due_date(45),'%B %e, %Y')} "
+		    f"if the contract was signed before October 1, 2019 or;"
+		    f"\n2.\t{datetime.datetime.strftime(due_date(60),'%B %e, %Y')} "
+		    f"if the contract was signed since then."
+		    f"\n"
+		    f"Follow link posted by the Daily Commercial News for more details:\n"
+		    f"{web_row.cert_url}"
+		    f"\n"
+		    f"Please be aware this is a fully automated message. "
+		    f"The info contained here could be erroneous."
 		)
-
+		
 		context = ssl.create_default_context()
 
 		try:
