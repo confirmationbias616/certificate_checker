ad29a0949f299086733b32f987e1dd536a61e990
Alex Roy <Alex@AlexMBP.local>
2019-02-23 19:39:29 -0500

    Now skipping the scraping code if results have already been scraped within the same day

--- weekly_script.py
+++ weekly_script.py
@@ -1,7 +1,13 @@
+import datetime
+import pandas as pd
 from scraper import scrape
 from wrangler import wrangle
 from matcher import match
 
 scrape()
+try:
+	pd.read_csv(f'./data/raw_web_certs_{datetime.datetime.now().date()}.csv')
+except FileNotFoundError:
+	scrape()
 wrangle()
 match()
