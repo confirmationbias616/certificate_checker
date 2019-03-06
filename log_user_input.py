a374e573855bfba9551f2277b5d44adbdfd8e743
Alex Roy <Alex@AlexMBP.local>
2019-03-05 22:50:51 -0500

    Update log_user_input.py

--- log_user_input.py
+++ log_user_input.py
@@ -3,51 +3,52 @@ import email
 import imaplib
 import json
 import pandas as pd
+import re
 
-imap_ssl_host = 'imap.gmail.com'
-imap_ssl_port = 993
-username = 'dilfo.hb.release'
-with open(".password.txt") as file: 
-	password = file.read()
 
-def get_job_input_data():
-    def parse_email(data):
-        for response_part in data:
-            if isinstance(response_part, tuple):
-                msg = email.message_from_string(response_part[1].decode('UTF-8'))
-                sender = msg['from']
-                for part in msg.walk():
-                    if part.get_content_type() == 'text/plain':
-                        content = part.get_payload(None, True).decode('UTF-8')
-                        break
-                    else:
-                        content = ''
-                return sender, content
-    server = imaplib.IMAP4_SSL(imap_ssl_host, imap_ssl_port)
-    server.login(username, password)
-    server.select('INBOX')
-    type, data = server.search(None, 'ALL') # UNSEEN or ALL
-    mail_ids = data[0]
-    id_list = mail_ids.split()
-    results = []
-    for i in id_list:
-        typ, data = server.fetch(i, '(RFC822)' )
-        sender, content = parse_email(data)
-        results.append({"sender":sender, "content":content})
-    server.logout()
-    return results
+def log_user_input():    
+    imap_ssl_host = 'imap.gmail.com'
+    imap_ssl_port = 993
+    username = 'dilfo.hb.release'
+    with open(".password.txt") as file: 
+        password = file.read()
+    def get_job_input_data():
+        def parse_email(data):
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(response_part[1].decode('UTF-8'))
+                    sender = msg['from']
+                    for part in msg.walk():
+                        if part.get_content_type() == 'text/plain':
+                            content = part.get_payload(None, True).decode('UTF-8')
+                            break
+                        else:
+                            content = ''
+                    return sender, content
+        server = imaplib.IMAP4_SSL(imap_ssl_host, imap_ssl_port)
+        server.login(username, password)
+        server.select('INBOX')
+        type, data = server.search(None, 'ALL')  # UNSEEN or ALL
+        mail_ids = data[0]
+        id_list = mail_ids.split()
+        results = []
+        for i in id_list:
+            typ, data = server.fetch(i, '(RFC822)')
+            sender, content = parse_email(data)
+            results.append({"sender": sender, "content": content})
+        server.logout()
+        return results
 
-file_path = './data/raw_dilfo_certs.csv'
-df = pd.read_csv(file_path)
+    file_path = './data/raw_dilfo_certs.csv'
+    df = pd.read_csv(file_path)
 
-for email in get_job_input_data():
-    try:
-    	dict_input = {x.split('=')[0]:x.split('=')[1] for x in email["content"].strip('\r\n').split('\r\n')}
-        dict_input.update({"receiver_email":email["sender"]})
-        df = df.append(dict_input, ignore_index=True)
-    except IndexError:
-        print(f'Could not process e-mail from {email["sender"]}')
+    for email_obj in get_job_input_data():
+        try:
+            dict_input = {x.split('=')[0]:x.split('=')[1] for x in re.compile('[\S ]*=[\S ]*').findall(email_obj['content'])}
+            dict_input.update({"receiver_email": re.compile('<?(\S+@\S+\.\w+)>?').findall(email_obj["sender"])[0].lower()})
+            df = df.append(dict_input, ignore_index=True)
+        except IndexError:
+            print(f'Could not process e-mail from {email_obj["sender"]}')
 
-df.drop_duplicates(subset=["address", "title", "owner"])
-df.to_csv(file_path, index=False)
-
+    df = df.dropna(thresh=7).drop_duplicates(subset=["address", "title", "owner"])
+    df.to_csv(file_path, float_format='string', index=False)
