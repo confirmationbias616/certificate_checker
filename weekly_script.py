2a156782c0475e08dfe1dc6cdfe69d57c5ce53e1
Alex Roy <Alex@AlexMBP.local>
2019-03-05 22:53:31 -0500

    Weekly script now logs new projects by scanning inbox

--- weekly_script.py
+++ weekly_script.py
@@ -1,13 +1,15 @@
 import datetime
 import pandas as pd
+from log_user_input import log_user_input
 from scraper import scrape
 from wrangler import wrangle
 from matcher import match
 
+log_user_input()
 try:
-	pd.read_csv(f'./data/raw_web_certs_{datetime.datetime.now().date()}.csv')
-	print("Already scraped today. Skipping on to wrangling!")
+    pd.read_csv(f'./data/raw_web_certs_{datetime.datetime.now().date()}.csv')
+    print("Already scraped today. Skipping on to wrangling!")
 except FileNotFoundError:
-	scrape()
+    scrape()
 wrangle()
 match()
