fd82508c91ae5d8ef4ab4c2b394927f1942e9601
Alex Roy <Alex@AlexMBP.local>
2019-03-07 17:13:18 -0500

    Tweaked mtching algorithms
    Still lots more tweaking left to do!

--- matcher.py
+++ matcher.py
@@ -17,17 +17,14 @@ def match(df_dilfo=False, df_web=False, test=False, threshold=0.9):
 				return fuzz.ratio(row, df_dilfo.iloc[i][attr])
 			except TypeError:
 				return 0
-		scoreable_attrs = ['contractor', 'street_name', 'street_number']
+		scoreable_attrs = ['contractor', 'street_name', 'street_number', 'title', 'city', 'owner']
 		for attr in scoreable_attrs:
 				df_web[f'{attr}_score'] = df_web[attr].apply(lambda row: attr_score(row, i, attr))
-		def multiply(row):
+		def compile_score(row):
 		    scores = row[[f'{attr}_score' for attr in scoreable_attrs]]
 		    scores = [x/100 for x in scores if type(x)==int]
-		    result = 1
-		    for number in scores:
-		        result = result*number
-		    return result
-		df_web['total_score'] = df_web.apply(lambda row: multiply(row), axis=1)
+		    return sum(scores)/len(scores)
+		df_web['total_score'] = df_web.apply(lambda row: compile_score(row), axis=1)
 		ranked = df_web.sort_values('total_score', ascending=False)
 		top_score = ranked.iloc[0]['total_score']
 		if top_score > threshold:
