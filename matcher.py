dceeb38609b4b15eeb96a7880cbce7d720b8f073
Alex Roy <Alex@AlexMBP.local>
2019-03-30 20:00:38 -0400

    Add partial_ratio features to training set

--- matcher.py
+++ matcher.py
@@ -12,16 +12,22 @@ def match(df_dilfo=False, df_web=False, test=False, min_score_thresh=0.66):
 		df_web = pd.read_csv(f'./data/clean_web_certs_{datetime.datetime.now().date()}.csv')
 	for i in range(len(df_dilfo)):
 		print(f"searching for potential match for project #{df_dilfo.iloc[i].job_number}...")
-		def attr_score(row, i, attr):
+		def attr_score(row, i, attr, seg='full'):
 			if row in ["", " ", "NaN", "nan", np.nan]:  # should not be comparing empty fields because empty vs empty is an exact match!
 				return 0
 			try:
-				return fuzz.ratio(row, df_dilfo.iloc[i][attr])
+				if seg=='full':
+					return fuzz.ratio(row, df_dilfo.iloc[i][attr])
+				else:
+					return fuzz.partial_ratio(row, df_dilfo.iloc[i][attr])
 			except TypeError:
 				return 0
 		scoreable_attrs = ['contractor', 'street_name', 'street_number', 'title', 'city', 'owner']
 		for attr in scoreable_attrs:
-				df_web[f'{attr}_score'] = df_web[attr].apply(lambda row: attr_score(row, i, attr))
+				df_web[f'{attr}_score'] = df_web[attr].apply(
+					lambda row: attr_score(row, i, attr, seg='full'))
+				df_web[f'{attr}_pr_score'] = df_web[attr].apply(
+					lambda row: attr_score(row, i, attr, seg='partial'))
 		def compile_score(row):
 		    scores = row[[f'{attr}_score' for attr in scoreable_attrs]]
 		    scores = [x/100 for x in scores if type(x)==int]
