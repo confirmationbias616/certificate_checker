392977d3c7ea859b314d3f6487813e5b8e34aa8f
Alex Roy <Alex@AlexMBP.local>
2019-05-04 22:18:03 -0400

    Accept db name as input to the create_connection function

--- db_tools.py
+++ db_tools.py
@@ -2,9 +2,9 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_connection():
+def create_connection(db_name='cert_db'):
     try:
-        conn = sqlite3.connect('cert_db')
+        conn = sqlite3.connect(db_name)
         return conn
     except Error as e:
         print(e)
