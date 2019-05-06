b2bf8fb38073ca426fe4a9fcc830943ddac64ef5
Alex Roy <Alex@AlexMBP.local>
2019-05-06 19:47:06 -0400

    Name test_cert_db properly when bringing into main project directory

--- test/test_setup.py
+++ test/test_setup.py
@@ -13,5 +13,5 @@ for csv_table_name in csv_file_names:
     with create_connection(db_name='test_cert_db') as conn:
         pd.read_csv(csv_table_name).to_sql(csv_table_name[:-4], conn, index=False)
 
-os.rename(abs_dir_path+"test/test_cert_db", abs_dir_path+"cert_db")
+os.rename(abs_dir_path+"test/test_cert_db", abs_dir_path+"test_cert_db")
 os.chdir(abs_dir_path)
