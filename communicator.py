89a8ab3633f4358587b0e522920dcb5f348e6408
Alex Roy <Alex@AlexMBP.local>
2019-05-22 20:49:07 -0400

    Resturcutred the preparation of email message

--- communicator.py
+++ communicator.py
@@ -14,7 +14,7 @@ log_handler.setFormatter(
 logger.addHandler(log_handler)
 logger.setLevel(logging.INFO)
 
-def communicate(web_row, dilfo_row, test=False):
+def communicate(web_df, dilfo_row, test=False):
 	port = 465 # for SSL
 	smtp_server = "smtp.gmail.com"
 	sender_email = "dilfo.hb.release"
@@ -40,10 +40,10 @@ def communicate(web_row, dilfo_row, test=False):
 
 	def send_email():
 		pud_date = datetime.datetime(
-				*[int(web_row.pub_date.split('-')[x]) for x in range(3)]).date()
+				*[int(web_df.iloc[0].pub_date.split('-')[x]) for x in range(3)]).date()
 		due_date = lambda delay: pud_date + datetime.timedelta(days=delay)
 		
-		message = (
+		intro_msg = (
 		    f"From: Dilfo HBR Bot"
 		    f"\n"
 		    f"To: {receiver_email}"
@@ -58,9 +58,17 @@ def communicate(web_row, dilfo_row, test=False):
 		    f"#{dilfo_row.job_number} - {dilfo_row.title} to the watchlist of upcoming "
 		    f"holdback releases.{cc_phrase}"
 		    f"\n"
-		    f"Before going any further, please follow the link below to make sure the "
-		    f"algorithm correctly matched the project in question:\n{web_row.cert_url}"
-		    f"\n"
+		)
+		if len(web_df) == 1:
+			enum_msg = (
+				f"Before going any further, please follow the link below to make sure the "
+		    	f"algorithm correctly matched the project in question:\n{web_row.cert_url}\n"
+			)
+		else:
+			enum_msg = "More than one possible match came up. Please check out each one to see if it's the right one.\n"
+			for i, link in enumerate(web_df.cert_url):
+				enum_msg += f'\tlink #{i+1}:\t{link}\n'
+		timing_msg = (
 		    f"If it's the right project, then the certificate was just "
 		    f"published this past {datetime.datetime.strftime(pud_date,'%A')} "
 		    f"on {datetime.datetime.strftime(pud_date,'%B %e, %Y')}. This means a "
@@ -70,9 +78,33 @@ def communicate(web_row, dilfo_row, test=False):
 		    f"\nB)\t{datetime.datetime.strftime(due_date(60),'%B %e, %Y')} "
 		    f"if the contract was signed since then."
 		    f"\n"
-		    f"Please be aware this is a fully automated message. "
-		    f"The info presented above could be erroneous."
 		)
+		disclaimer_msg = (
+		    "Please be aware this is a fully automated message. "
+		    "The info presented above could be erroneous."
+		)
+		if len(web_df) == 1:
+			feedback_msg = (
+				"You can help improve the matching algorithms by replying to "
+				"this e-mail with a simple `1` or `0` to confirm whether or "
+				"not the linked certificate represents the project in question."
+				"\n"
+			)
+		else:
+			feedback_msg = (
+				"You can help improve the matching algorithms by replying to "
+				"this e-mail with the number from above which corresponds to "
+				"the correct link for your project. For example, just reply `1` "
+				"if the first link was the right one. If none of the links were "
+				"correct matches, please reply `0`."
+				"\n"
+			)
+		closeout_msg = (
+			"Thanks,\n"
+			"Dilfo HBR Bot\n"
+		)
+
+		message = '\n'.join([intro_msg, enum_msg, timing_msg, disclaimer_msg, feedback_msg, closeout_msg])
 
 		if not test:
 			try:
