fcd6ced4f0e2595ef3ba37972ffc8f6935b08d8b
Alex Roy <confirmationbias616>
2019-07-31 17:40:18 -0400

    Update match notification message to reflect latest changes

--- communicator.py
+++ communicator.py
@@ -55,9 +55,8 @@ def communicate(web_df, dilfo_row, test=False):
 		    f"\n\n"
 		    f"Hi {receiver_email.split('.')[0].title()},"
 		    f"\n\n"
-		    f"You're receiving this e-mail notification because you added the project "
-		    f"#{dilfo_row.job_number} - {dilfo_row.title} to the watchlist of upcoming "
-		    f"holdback releases."
+		    f"It looks like your project #{dilfo_row.job_number} - "
+			f"{dilfo_row.title} might be almost ready for holdback release!"
 		    f"\n"
 		)
 		if len(web_df) == 1:
@@ -70,8 +69,7 @@ def communicate(web_df, dilfo_row, test=False):
 			for i, dcn_key in enumerate(web_df.dcn_key,1):
 				enum_msg += f'\tlink #{i}:\t{lookup_url}{dcn_key}\n'
 		timing_msg = (
-		    f"If it's the right project, then the certificate was just "
-		    f"published this past {datetime.datetime.strftime(pub_date,'%A')} "
+		    f"If it's the right project, then the certificate was just published "
 		    f"on {datetime.datetime.strftime(pub_date,'%B %e, %Y')}. This means a "
 		    f"valid holdback release invoice could be submitted as of:"
 		    f"\nA)\t{datetime.datetime.strftime(due_date(45),'%B %e, %Y')} "
@@ -83,31 +81,30 @@ def communicate(web_df, dilfo_row, test=False):
 		disclaimer_msg = (
 		    "Please be aware this is a fully automated message. "
 		    "The info presented above could be erroneous."
+			"\n"
 		)
 		if len(web_df) == 1:
 			feedback_msg = (
-				"You can help improve the matching algorithms by replying to "
-				"this e-mail with a simple `1` or `0` to confirm whether or "
-				"not the linked certificate represents the project in question."
+				"Your feedback will be required so the service can properly "
+				"handle this ticket, whether that means closing it out or keep "
+				"searching for new matches. It will also help improve the "
+				"matching algorithm for future projects.\n"
+				"Please respond to this email with one of the numbers from below, "
+				"which corrsponds to your situation with the most accuracy:\n"
+				"\t0: did not relate to my project\n"
+				"\t1: was accurate match for my project\n"
+				"\t2: was close but seemed to represent a different phase or "
+				"stage of my project.\n"
 				"Whether you were the main receiver or just in CC, your"
 				"feedback is greatly apreciated."
-				"\n"
-			)
-		else:
-			feedback_msg = (
-				"You can help improve the matching algorithms by replying to "
-				"this e-mail with the number from above which corresponds to "
-				"the correct link for your project. For example, just reply `1` "
-				"if the first link was the right one. If none of the links were "
-				"correct matches, please reply `0`."
-				"\n"
+				"\n"	
 			)
 		closeout_msg = (
 			"Thanks,\n"
 			"Dilfo HBR Bot\n"
 		)
 
-		message = '\n'.join([intro_msg, enum_msg, timing_msg, disclaimer_msg, feedback_msg, closeout_msg])
+		message = '\n'.join([intro_msg, enum_msg, timing_msg, feedback_msg, disclaimer_msg, closeout_msg])
 
 		if not test:
 			try:
