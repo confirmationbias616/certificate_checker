9b834ed303f606ed7714111c33e169a35fb6f433
Alex Roy <confirmationbias616>
2019-09-10 22:01:02 -0400

    Modify daily_script to exclude training and validating
    temp fix while certain refactoring is taking place

--- daily_script.py
+++ daily_script.py
@@ -27,14 +27,14 @@ def daily_routine(exit_if_stale=False):
     fruitful_scraping = scrape_dcn()  # returns True or False
     if not fruitful_scraping and exit_if_stale:
         return  # short-ciruit out since new new data has been collected
-    logger.info('build_train_set')
-    build_train_set()
-    logger.info('train_model')
-    train_model(prob_thresh=prob_thresh)
+    # logger.info('build_train_set')
+    # build_train_set()
+    # logger.info('train_model')
+    # train_model(prob_thresh=prob_thresh)
     logger.info('match')
     match(prob_thresh=prob_thresh)  #test=True to mute sending of e-mails
-    logger.info('validate')
-    validate_model(prob_thresh=prob_thresh)
+    # logger.info('validate')
+    # validate_model(prob_thresh=prob_thresh)
 
 if __name__=="__main__":
     for filename in ['cert_db.sqlite3', 'rf_model.pkl', 'rf_features.pkl']:
