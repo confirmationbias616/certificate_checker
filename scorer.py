ac5451f42fb2b5728d0d27578d66d1f9df8adf2f
Alex Roy <confirmationbias616>
2019-09-19 22:32:12 -0400

    Rename variables for clarity and improve data validation logic

--- scorer.py
+++ scorer.py
@@ -31,7 +31,12 @@ def attr_score(web_str, company_project_str, match_style="full"):
     except TypeError:
         return 0
 
-def build_match_score(company_projects, df_web):
+def build_match_score(single_project_df, web_df):
+    if len(single_project_df) > 1:
+        raise ValueError(
+            f"`company_projects` dataframe was suppose to conatin only 1 single row - "
+            f"it contained {len(single_project_df)} rows instead."
+        )
     scoreable_attrs = [
         "contractor",
         "street_name",
@@ -40,17 +45,18 @@ def build_match_score(company_projects, df_web):
         "city",
         "owner",
     ]
+    possible_matches_scored = web_df  # renaming for clarity
     for (
         _,
         company_project_row,
     ) in (
-        company_projects.iterrows()
+        single_project_df.iterrows()
     ):  # nescessary even when company_projects is single row because it's still DataFrame instead of Series
         for attr in scoreable_attrs:
             for attr_suffix, match_style in zip(
                 ["score", "pr_score"], ["full", "partial"]
             ):
-                df_web[f"{attr}_{attr_suffix}"] = df_web.apply(
+                possible_matches_scored[f"{attr}_{attr_suffix}"] = possible_matches_scored.apply(
                     lambda web_row: attr_score(
                         web_row[attr],
                         company_project_row[attr],
@@ -58,10 +64,7 @@ def build_match_score(company_projects, df_web):
                     ),
                     axis=1,
                 )
-        df_web["total_score"] = df_web.apply(
-            lambda row: compile_score(row, scoreable_attrs), axis=1
+        possible_matches_scored["total_score"] = possible_matches_scored.apply(
+            lambda row: compile_score(row, scoreable_attrs, 'multiply'), axis=1
         )
-        if (
-            len(company_projects) == 1
-        ):  # if single row was passed instead of actual dataframe
-            return df_web
+        return possible_matches_scored
