cb3715d814d58447a7ede6a1796f0c340916fe7c
Alex Roy <confirmationbias616>
2019-10-30 21:12:14 -0400

    Troubleshoot prod bug (attempt #2)

--- matcher.py
+++ matcher.py
@@ -264,6 +264,7 @@ def match(
             results_master = results_master.append(results)
         except NameError:
             results_master = results
+    print(results_master)
     logger.info(
         f"Done looping through {len(company_projects)} open projects. Sent {comm_count} "
         f"e-mails to communicate matches as a result."
