d3eadc052dfa0e34ab475d6afd3912e55cee3f6e
Alex Roy <confirmationbias616>
2019-11-28 22:15:28 -0500

    Wrap everything in try/except block to prevent anything from taking down the pipeline
    Not convinced this is a good idea yet. We'll see how it performs in prod.

--- scripts/daily_routine.py
+++ scripts/daily_routine.py
@@ -23,27 +23,39 @@ def daily_routine(exit_if_stale=False):
     logger.info("initiating daily routine...")
     prob_thresh = load_config()["machine_learning"]["prboability_thresholds"]["general"]
     if "scrape" in load_config()["daily_routine"]["steps"]:
-        sources_scraped = []
-        for source in load_config()["daily_routine"]["scrape_source"]:
-            logger.info(f"scrape {source}")
-            source_scraped = scrape(
-                source=source
-            )  # bool whether or not CSP's were retreived
-            sources_scraped.append(source_scraped)
-        if not any(sources_scraped) and load_config()["daily_routine"]["exit_if_stale"]:
-            logger.info("No new CSP certificates today. Exiting early.")
-            return  # short-ciruit out since new new CSP's has been collected
+        try:
+            sources_scraped = []
+            for source in load_config()["daily_routine"]["scrape_source"]:
+                logger.info(f"scrape {source}")
+                source_scraped = scrape(
+                    source=source
+                )  # bool whether or not CSP's were retreived
+                sources_scraped.append(source_scraped)
+            if not any(sources_scraped) and load_config()["daily_routine"]["exit_if_stale"]:
+                logger.info("No new CSP certificates today. Exiting early.")
+                return  # short-ciruit out since new new CSP's has been collected
+        except Exception as e:
+            logger.critical(e)
     if "train" in load_config()["daily_routine"]["steps"]:
-        logger.info("build_train_set")
-        build_train_set()
-        logger.info("train_model")
-        train_model(prob_thresh=prob_thresh)
+        try:
+            logger.info("build_train_set")
+            build_train_set()
+            logger.info("train_model")
+            train_model(prob_thresh=prob_thresh)
+        except Exception as e:
+            logger.critical(e)
     if "validate" in load_config()["daily_routine"]["steps"]:
-        logger.info("validate")
-        validate_model(prob_thresh=prob_thresh)
+        try:
+            logger.info("validate")
+            validate_model(prob_thresh=prob_thresh)
+        except Exception as e:
+            logger.critical(e)
     if "match" in load_config()["daily_routine"]["steps"]:
-        logger.info("match")
-        match(prob_thresh=prob_thresh, test=load_config()["daily_routine"]["test"])
+        try:
+            logger.info("match")
+            match(prob_thresh=prob_thresh, test=load_config()["daily_routine"]["test"])
+        except Exception as e:
+            logger.critical(e)
 
 
 if __name__ == "__main__":
