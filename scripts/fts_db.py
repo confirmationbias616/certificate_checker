6049d22f465a28c2288d97d179e3ae815c253d54
Alex Roy <confirmationbias616>
2019-12-16 22:24:55 -0500

    Fix relative import from parent directory

--- scripts/fts_db.py
+++ scripts/fts_db.py
@@ -1,5 +1,10 @@
-from utils import create_connection
 import sqlite3
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from utils import create_connection
 
 
 rebuild_fts = """
