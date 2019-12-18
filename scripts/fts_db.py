d65443f5b490cb2f2c9342face6b80bfe1885cbc
Alex Roy <confirmationbias616>
2019-12-17 20:11:27 -0500

    Drop fts table to replace from scratch instead of rebuilding

--- scripts/fts_db.py
+++ scripts/fts_db.py
@@ -7,8 +7,8 @@ sys.path.insert(0,parentdir)
 from utils import create_connection
 
 
-rebuild_fts = """
-    INSERT INTO cert_search(cert_sarch) VALUES('rebuild');
+drop_fts = """
+    DROP TABLE cert_search
 """
 create_fts = """
     CREATE VIRTUAL TABLE cert_search
@@ -16,17 +16,18 @@ create_fts = """
 """
 populate_fts = """
     INSERT INTO cert_search
-    SELECT cert_id, title || ' ' || owner || ' ' || contractor || ' ' || city as text FROM web_certificates; 
+    SELECT cert_id, title || ' ' || owner || ' ' || contractor || ' ' || city as text FROM web_certificates;
 """
 
 def create_or_update_fts():
     try:
         with create_connection() as conn:
-            conn.cursor().execute(rebuild_fts)
+            conn.cursor().execute(drop_fts)
     except sqlite3.OperationalError:
-        with create_connection() as conn:
-            conn.cursor().execute(create_fts)
-            conn.cursor().execute(populate_fts)
+        pass
+    with create_connection() as conn:
+        conn.cursor().execute(create_fts)
+        conn.cursor().execute(populate_fts)
 
 if __name__ == "__main__":
     create_or_update_fts()
