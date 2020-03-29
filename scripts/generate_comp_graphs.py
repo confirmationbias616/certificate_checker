e4091698cef8e94c6965ad664917e54c0031f8b9
Alex Roy <confirmationbias616>
2020-03-29 15:23:45 -0400

    Reduce minimum width of graph

--- scripts/generate_comp_graphs.py
+++ scripts/generate_comp_graphs.py
@@ -28,7 +28,7 @@ df_agg = pd.DataFrame(df_agg.to_records())
 for source in ['dcn', 'ocn', 'l2b']:
     df_agg[f'{source}_p'] = df_agg.apply(lambda row: row[f'{source}'] * 100 / sum([row['dcn'], row['ocn'], row['l2b']]), axis=1)
 
-plt.figure(figsize=[15,10])
+plt.figure(figsize=[10,10])
 plt.rcParams.update({'font.size': 22})
 plt.bar(df_agg.yearmonth, df_agg.dcn, bottom=df_agg.ocn+df_agg.l2b, align='center', alpha=0.2, label='Daily Commercial News', color='gray')
 plt.bar(df_agg.yearmonth, df_agg.ocn, bottom=df_agg.l2b, align='center', alpha=0.5, label='Ontario Construction News', color='blue')
@@ -42,7 +42,7 @@ for spine in ax.spines:
 ax.tick_params(axis=u'both', which=u'both',length=0)
 ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-plt.xticks(['2018-04', '2018-08', '2018-12', '2019-04', '2019-08', '2019-12', '2020-04'])
+plt.xticks(['2018-06', '2018-12', '2019-06', '2019-12', '2020-06'])
 plt.locator_params(axis='x', nbins=20)
 legend = plt.legend(frameon=1, prop={'size': 20})
 frame = legend.get_frame()
